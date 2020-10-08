9463cb835df72fad95d6e4dce8f5f7868d398c33
st3107 <st3107@columbia.edu>
2020-10-08 14:43:46 -0400

    TST: update configure

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,51 +4,65 @@ import pyFAI
 import pytest
 from diffpy.pdfgetx import PDFConfig, PDFGetter
 from diffpy.structure import loadStructure
+from pdfstream.io import load_img, load_array
 from pkg_resources import resource_filename
 from pyobjcryst import loadCrystal
 from pyobjcryst.molecule import Molecule
 
-from pdfstream.io import load_img, load_data
-
-NI_PONI = resource_filename('tests', 'test_data/Ni_poni_file.poni')
-NI_GR = resource_filename('tests', 'test_data/Ni_gr_file.gr')
-NI_CHI = resource_filename('tests', 'test_data/Ni_chi_file.chi')
-NI_FGR = resource_filename('tests', 'test_data/Ni_fgr_file.fgr')
-NI_IMG = resource_filename('tests', 'test_data/Ni_img_file.tiff')
-NI_CIF = resource_filename('tests', 'test_data/Ni_cif_file.cif')
-KAPTON_IMG = resource_filename('tests', 'test_data/Kapton_img_file.tiff')
-BLACK_IMG = resource_filename('tests', 'test_data/black_img.tiff')
-WHITE_IMG = resource_filename('tests', 'test_data/white_img.tiff')
+# data file
+NI_PONI_FILE = resource_filename('tests', 'test_data/Ni_poni_file.poni')
+NI_GR_FILE = resource_filename('tests', 'test_data/Ni_gr_file.gr')
+NI_CHI_FILE = resource_filename('tests', 'test_data/Ni_chi_file.chi')
+NI_FGR_FILE = resource_filename('tests', 'test_data/Ni_fgr_file.fgr')
+NI_IMG_FILE = resource_filename('tests', 'test_data/Ni_img_file.tiff')
+MASK_FILE = resource_filename("tests", "test_data/mask_file.npy")
+KAPTON_IMG_FILE = resource_filename('tests', 'test_data/Kapton_img_file.tiff')
+BLACK_IMG_FILE = resource_filename('tests', 'test_data/black_img.tiff')
+WHITE_IMG_FILE = resource_filename('tests', 'test_data/white_img.tiff')
+NI_IMG = load_img(NI_IMG_FILE)
+KAPTON_IMG = load_img(KAPTON_IMG_FILE)
+NI_GR = load_array(NI_GR_FILE)
+NI_CHI = load_array(NI_CHI_FILE)
+NI_FGR = load_array(NI_FGR_FILE)
 NI_CONFIG = PDFConfig()
-NI_CONFIG.readConfig(NI_GR)
+NI_CONFIG.readConfig(NI_GR_FILE)
 NI_PDFGETTER = PDFGetter(NI_CONFIG)
-ZRP_CIF = resource_filename('tests', 'test_data/ZrP.cif')
-NI_CRYSTAL = loadCrystal(NI_CIF)
-ZRP_CRYSTAL = loadCrystal(ZRP_CIF)
-NI_DIFFPY = loadStructure(NI_CIF)
+AI = pyFAI.load(NI_PONI_FILE)
+MASK = numpy.load(MASK_FILE)
+BLACK_IMG = load_img(BLACK_IMG_FILE)
+WHITE_IMG = load_img(WHITE_IMG_FILE)
+# model file
+ZRP_CIF_FILE = resource_filename('tests', 'test_data/ZrP_cif_file.cif')
+NI_CIF_FILE = resource_filename("tests", "test_data/Ni_cif_file.cif")
+NI_CRYSTAL = loadCrystal(NI_CIF_FILE)
+ZRP_CRYSTAL = loadCrystal(ZRP_CIF_FILE)
+NI_DIFFPY = loadStructure(NI_CIF_FILE)
+NI_MOLECULE = Molecule(NI_CRYSTAL)
 
 DB = {
-    'Ni_img_file': NI_IMG,
-    'Ni_img': load_img(NI_IMG),
-    'Kapton_img_file': KAPTON_IMG,
-    'Kapton_img': load_img(KAPTON_IMG),
-    'Ni_poni_file': NI_PONI,
-    'Ni_gr_file': NI_GR,
-    'Ni_chi_file': NI_CHI,
-    'Ni_fgr_file': NI_FGR,
-    'ai': pyFAI.load(NI_PONI),
-    'Ni_gr': load_data(NI_GR).T,
-    'Ni_chi': load_data(NI_CHI).T,
-    'Ni_fgr': load_data(NI_FGR).T,
-    'black_img_file': BLACK_IMG,
-    'white_img_file': WHITE_IMG,
-    'black_img': numpy.zeros((128, 128)),
-    'white_img': numpy.ones((128, 128)),
+    'Ni_img_file': NI_IMG_FILE,
+    'Ni_img': NI_IMG,
+    'Kapton_img_file': KAPTON_IMG_FILE,
+    'Kapton_img': KAPTON_IMG,
+    'Ni_poni_file': NI_PONI_FILE,
+    'Ni_gr_file': NI_GR_FILE,
+    'Ni_chi_file': NI_CHI_FILE,
+    'Ni_fgr_file': NI_FGR_FILE,
+    'ai': AI,
+    'Ni_gr': NI_GR,
+    'Ni_chi': NI_CHI,
+    'Ni_fgr': NI_FGR,
+    'black_img_file': BLACK_IMG_FILE,
+    'white_img_file': WHITE_IMG_FILE,
+    'black_img': BLACK_IMG,
+    'white_img': WHITE_IMG,
     'Ni_config': NI_CONFIG,
     'Ni_pdfgetter': NI_PDFGETTER,
-    'Ni_stru_file': NI_CIF,
+    'mask_file': MASK_FILE,
+    'mask': MASK,
+    'Ni_stru_file': NI_CIF_FILE,
     'Ni_stru': NI_CRYSTAL,
-    'Ni_stru_molecule': Molecule(NI_CRYSTAL),
+    'Ni_stru_molecule': NI_MOLECULE,
     'Ni_stru_diffpy': NI_DIFFPY,
     'ZrP_stru': ZRP_CRYSTAL
 }
--- tests/modeling/conftest.py
+++ tests/modeling/conftest.py
@@ -7,14 +7,14 @@ from pdffitx.modeling.adding import initialize
 from pdffitx.modeling.creating import create
 from pdffitx.modeling.fitobjs import MyParser
 from pdffitx.modeling.main import multi_phase
-from tests.conftest import NI_GR, NI_CIF
+from tests.conftest import NI_GR, NI_CIF_FILE
 
 
 @pytest.fixture(scope="function", params=[loadCrystal, loadStructure])
 def recipe(request):
     parser = MyParser()
     parser.parseFile(NI_GR)
-    stru = request.param(NI_CIF)
+    stru = request.param(NI_CIF_FILE)
     recipe = multi_phase([(F.sphericalCF, stru)], parser, fit_range=(2., 8.0, .1), values={
         'psize_G0': 200}, sg_params={'G0': 225})
     return recipe
