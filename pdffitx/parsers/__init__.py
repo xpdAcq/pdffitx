bd2adcd90f355e107233ee8c92bff5a9c7dabd1b
st3107 <st3107@columbia.edu>
2020-10-08 14:27:35 -0400

    MNT: delete gemmi related code

--- pdffitx/parsers/__init__.py
+++ pdffitx/parsers/__init__.py
@@ -1,12 +1,11 @@
 from .atoms import dict_to_atoms, dict_to_atoms2
-from .ciffile import cif_to_dict, to_crystal
+from .ciffile import to_crystal
 from .fitdata import dict_to_array, dicts_to_array
 from .fitrecipe import recipe_to_dict
 from .fitrecipe import recipe_to_dict2
 from .fitresult import rename_rule, to_latex, to_xarray
 
 dict_to_atoms = dict_to_atoms
-cif_to_dict = cif_to_dict
 dict_to_array = dict_to_array
 dicts_to_array = dicts_to_array
 recipe_to_dict = recipe_to_dict
--- tests/parsers/test_ciffile.py
+++ tests/parsers/test_ciffile.py
@@ -1,30 +1,5 @@
-from pprint import pprint
-
-import pytest
-from mongomock import MongoClient
+from pdfstream.parsers.ciffile import to_crystal
 from pyobjcryst.crystal import Crystal
-
-from pdfstream.parsers.ciffile import cif_to_dict, to_crystal
-
-
-@pytest.mark.parametrize(
-    "kwargs",
-    [
-        {"mmjson": True},
-        {"mmjson": False}
-    ]
-)
-def test_cif_to_dict(db, kwargs):
-    client = MongoClient()
-    coll = client.db.coll
-    dcts = cif_to_dict(
-        db["Ni_stru_file"],
-        **kwargs
-    )
-    for dct in dcts:
-        pprint(dct)
-        # test mongo friendly
-        coll.insert_one(dct)
 
 
 def test_to_crystal(doc2):
